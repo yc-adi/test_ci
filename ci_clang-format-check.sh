e9086f622ae8c62768571537687dca3f606888a7
Ying Cai <ying.cai@analog.com>
2022-10-17 09:08:02 -0500

    Added clang format check.

---
+++ ci_clang-format-check.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+echo pwd=$(pwd)
+
+cat .clang-format
+
+CHANGE_FILES=$(git diff --ignore-submodules --name-only remotes/origin/main '*.c' '*.h' ':!*_regs.h')
+
+echo CHANGE_FILES=$CHANGE_FILES
+
+CHECK_FAIL=0
+
+set +e
+
+for change_file in ${CHANGE_FILES}
+do
+    if [ -f ${change_file} ];
+    then
+        clang-format --verbose --style=file -n -Werror ${change_file}
+        if [ $? != 0 ];
+        then
+            clang-format --style=file ${change_file} > ${change_file}.clang
+            diff -u --color=always ${change_file} ${change_file}.clang
+            rm ${change_file}.clang
+            CHECK_FAIL=1
+        fi
+    fi
+done
+set -e
+
+
+echo "\nDone with $CHECK_FAIL."
+
+exit $CHECK_FAIL
